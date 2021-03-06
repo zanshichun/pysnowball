3164392f257a73686606e63aba4483ab76c488e0
zanshichun <zsc9602@163.com>
2021-03-06 22:21:32 +0800

    update:cons

--- pysnowball/cons.py
+++ pysnowball/cons.py
@@ -1,3 +1,7 @@
 
 NOT200_ERROR_MSG = "请求错误"
 NOTOKEN_ERROR_MSG = "未设置TOKEN"
+
+MAIL_HOST = "smtp.qq.com"
+MAIL_USER = "发送者"
+MAIL_PASS = "密码"
--- pysnowball/emailutils.py
+++ pysnowball/emailutils.py
@@ -2,24 +2,19 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 from smtplib import SMTP_SSL
-
-MAIL_HOST = "smtp.qq.com"
-MAIL_USER = ""
-MAIL_PASS = ""
-
-MAIL_SENDER = ""
+from pysnowball import cons
 
 # 将每日盈亏情况发送到邮箱
 def send_email_alert(recipient,ytdpl_val,purchased_val):
     MAIL_MESSAGE=MIMEText("较昨日盈亏：\t"+ytdpl_val+"\n"+"较购入盈亏值：\t"+purchased_val+"\n",'plain','utf-8')
     receivers = [recipient]
     try:
-        smtpObj = SMTP_SSL(MAIL_HOST)
-        smtpObj.ehlo(MAIL_HOST)
+        smtpObj = SMTP_SSL(cons.MAIL_HOST)
+        smtpObj.ehlo(cons.MAIL_HOST)
         # 登录到服务器
-        smtpObj.login(MAIL_USER, MAIL_PASS)
+        smtpObj.login(cons.MAIL_USER, cons.MAIL_PASS)
         # 发送
-        smtpObj.sendmail(MAIL_SENDER, receivers, MAIL_MESSAGE.as_string())
+        smtpObj.sendmail(cons.MAIL_USER, receivers, MAIL_MESSAGE.as_string())
         # 退出
         smtpObj.quit()
         print('success')
